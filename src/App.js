f953f330d365339696656342298095ca8160440a
pramod <pramodjnrdhn@gmail.com>
2021-03-15 15:16:44 +0530

    added header portion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        Header
+        <Header />
       </div>
       <div className="search-brand">
         brand search
---
+++ src/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css';
+import ProfilePicture from '../Assets/profile.png';
+import ShoppingBag from '../Assets/shopping-bag.png';
+
+
+const  imageStyle = {
+    width: "20px",
+    color: "rgb(103,11,25)"
+}
+
+
+function Header () {
+
+    return(
+        <>
+            <div class="topnav">
+                <a href="#Contact us">Contact Us   |</a> 
+                <a href="#returns">7 Day Returns  |</a> 
+                <a href="#track">Track Order  |</a> 
+                <a href="#return">Return Order  |</a> 
+                <a href="#sell">Sell On Mirraw  |</a> 
+                <a href="#survey">Survey</a>
+                <div class="topnav-right">
+                    <a href="#profile"><img style={imageStyle} src={ProfilePicture} alt="profile" />LOG IN</a>
+                    <a href="#cart"><img style={imageStyle} src={ShoppingBag} alt="cart" />CART(0)</a>
+                </div>
+            </div>
+                        
+        </>
+    );
+}
+
+export default Header;
